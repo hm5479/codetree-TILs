3c7ba6571e6c4b5310242f2bfbb860f4b9eafa36
88 <164597360+hm5479@users.noreply.github.com>
2024-11-06 19:41:57 +0900

    Title: Date to Date, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241106/Date to Date/date-to-date.cpp
+++ 241106/Date to Date/date-to-date.cpp
@@ -4,17 +4,18 @@ using namespace std;
 int main() {
     int m1, d1, m2, d2;
     cin >> m1 >> d1 >> m2 >> d2;
-    int elapsed_day = 0;
-    int endOfMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int elapsed_day = 1;
+    int endOfMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     while (true) {
+$0
+        if (d1 == d2 && m1 == m2) break;
+
         d1++;
         elapsed_day++;
 
-        if (d1 == d2 && m1 == m2) break;
-        
-        if(d1 == endOfMonth[m1]){
-            d1 = 0;
+        if(d1 > endOfMonth[m1]){
+            d1 = 1;
             m1++;
         }
     }
