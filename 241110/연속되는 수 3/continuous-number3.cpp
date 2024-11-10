ef3bd17e4013bb679e984af55780b973ca399315
88 <164597360+hm5479@users.noreply.github.com>
2024-11-10 18:53:47 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241110/연속되는 수 3/continuous-number3.cpp
+++ 241110/연속되는 수 3/continuous-number3.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 int main() {
@@ -14,9 +14,10 @@ int main() {
     for (int i = 0; i < n; i++) {
         if (i != 0 && arr[i] * arr[i - 1] > 0) {
             tmp++;
-            ans = max(tmp,ans);
         } else
             tmp = 1;
+
+        ans = max(tmp, ans);
     }
 
     cout << ans;
