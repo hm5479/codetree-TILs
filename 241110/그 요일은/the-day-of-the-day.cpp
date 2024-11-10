8a59fe7a1eb58d2e911e661cacab639203b36f3e
88 <164597360+hm5479@users.noreply.github.com>
2024-11-10 16:51:13 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241110/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int getDays(int m, int d) {
+    int dayOfMonth[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    int days = 0;
+    for (int i = 1; i < m; i++) {
+        days += dayOfMonth[i];
+    }
+    days += d;
+    return days;
+}
+
+int main() {
+    int m1, d1, m2, d2;
+    string yoil;
+    cin >> m1 >> d1 >> m2 >> d2 >> yoil;
+    string yoils[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+    int days1 = getDays(m1, d1);
+    int days2 = getDays(m2, d2);
+
+    int diff = days2 - days1;
+    int yoilNum;
+
+    for (int i = 0; i < 7; i++) {
+        if (yoil == yoils[i]) yoilNum = i;
+    }
+
+    int tot = (diff - yoilNum) / 7 + 1;
+    cout << tot;
+}
