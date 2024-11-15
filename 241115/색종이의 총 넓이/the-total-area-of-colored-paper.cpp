a60359abb74f61ddf0b72eb23b592cae45443253
88 <164597360+hm5479@users.noreply.github.com>
2024-11-15 15:46:41 +0900

    Title: 색종이의 총 넓이, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241115/색종이의 총 넓이/the-total-area-of-colored-paper.cpp
+++ 241115/색종이의 총 넓이/the-total-area-of-colored-paper.cpp
@@ -8,9 +8,9 @@ using namespace std;
 int arr[MAX_M][MAX_M] = {0};
 
 void paintArr(int row, int col) {
-    for (int r = row - LENGTH + 1; r <= row; r++) {
-        for (int c = col; c < col + LENGTH; c++) {
-            arr[r][c] = 1;
+    for (int i = row; i < row + LENGTH; i++) {
+        for (int j = col; j < col + LENGTH; j++) {
+            arr[i][j] = 1;
         }
     }
 }
@@ -24,13 +24,13 @@ int main() {
         cin >> r >> c;
         r += OFFSET;
         c += OFFSET;
-        paintArr(r,c);
+        paintArr(r, c);
     }
 
     int ans = 0;
-    for(int i=0; i<MAX_M; i++){
-        for(int j=0; j<MAX_M; j++)
-            if(arr[i][j] == 1) ans++;
+    for (int i = 0; i < MAX_M; i++) {
+        for (int j = 0; j < MAX_M; j++)
+            if (arr[i][j] == 1) ans++;
     }
 
     cout << ans;
