b84a18b61bb23a920dec35ebb86ff041ddf7503d
88 <164597360+hm5479@users.noreply.github.com>
2024-11-16 17:49:02 +0900

    Title: 특정 구간의 원소 평균값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241116/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
+++ 241116/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -2,30 +2,30 @@
 #include <iostream>
 using namespace std;
 
-#define MAX_M 10000
-
 int main() {
     int n;
-    int arr[n];
-
     cin >> n;
+
+    int arr[n];
     for (int i = 0; i < n; i++) cin >> arr[i];
 
     int cnt = 0;
     for (int i = 0; i < n; i++) {
         for (int j = i; j < n; j++) {
-            
-            int sum = 0;
-            for (int k = i; k <= j; k++)
-                sum += arr[k];
+            if(i==j) cnt++;
 
-            int average = sum / (j - i + 1);
+            else {
+                int sum = 0;
+                for (int k = i; k <= j; k++)
+                    sum += arr[k];
 
-            for (int k = i; k <= j; k++)
-                if(arr[k]== average){
-                    cnt++;
-                    break;
-                }
+                double average = (double) sum / (double) (j - i + 1);
+                for (int k = i; k <= j; k++)
+                    if((double)arr[k] == average){
+                        cnt++;
+                        break;
+                    }
+            }
         }
     }
 
