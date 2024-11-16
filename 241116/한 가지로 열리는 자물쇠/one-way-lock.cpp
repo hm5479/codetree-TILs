71675f026e6753a6fb29a16572e33670a978a563
88 <164597360+hm5479@users.noreply.github.com>
2024-11-16 18:09:10 +0900

    Title: 한 가지로 열리는 자물쇠, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241116/한 가지로 열리는 자물쇠/one-way-lock.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[3];
+    for (int i = 0; i < 3; i++) cin >> arr[i];
+
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            for (int k = 1; k <= n; k++) {
+                bool flag = false;
+
+                if (i >= arr[0] - 2 && i <= arr[0] + 2 
+                || j >= arr[1] - 2 && j <= arr[1] + 2 
+                || k >= arr[2] - 2 && k <= arr[2] + 2) flag = true;
+
+                if (flag) cnt++;
+            }
+        }
+    }
+    cout << cnt;
+}
+$0
