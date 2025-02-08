dbc282523d862411d614ebc146c8a7305a016b1e
hm5479 <164597360+hm5479@users.noreply.github.com>
2025-02-08 16:08:01 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250208/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int getDays(int m, int d) {
+    int numOfDays[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int day = d;
+    for (int i = 1; i < m; i++)
+        day += numOfDays[i];
+    return day;
+}
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;  // 일 시간 분
+
+    int day = getDays(11, A) - getDays(11, 11);
+    cout << (day * 24 * 60) - (11 * 60 + 11) + (B * 60 + C);
+}
